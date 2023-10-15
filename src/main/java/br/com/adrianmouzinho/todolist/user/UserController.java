80388048ffbc7cb50736e0b6c407e036c6185980
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2023-10-15 09:35:07 -0300

    Aulas 03 e 04

--- src/main/java/br/com/adrianmouzinho/todolist/user/UserController.java
+++ src/main/java/br/com/adrianmouzinho/todolist/user/UserController.java
@@ -8,24 +8,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
   @Autowired
   private IUserRepository userRepository;
-  
+
   @PostMapping("/")
   public ResponseEntity create(@RequestBody UserModel userModel) {
     var userExists = this.userRepository.findByUsername(userModel.getUsername());
 
-    if(userExists != null) {
+    if (userExists != null) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
     }
 
-    var createdUser = this.userRepository.save(userModel);
+    var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
 
-    return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    userModel.setPassword(passwordHashed);
+
+    var user = this.userRepository.save(userModel);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(user);
   }
 
 }
